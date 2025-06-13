1cb1b984ad8a565d7985c2298add960f1f6d5ea8
fatima1414 <shaikhfatima5260@gmail.com>
2025-06-13 13:19:11 +0530

    code

--- Empty_input/form_input_error/script.js
+++ Empty_input/form_input_error/script.js
@@ -1,74 +1,91 @@
 const signup = document.querySelector("#signup")
 signup.addEventListener('submit', (e) => {
     e.preventDefault()
+
+
+
+    //////All input  Data 
+    const name = document.querySelector("#name").value
+    const email = document.querySelector("#email").value
+    const mobile = document.querySelector("#mobile").value
+    const city = document.querySelector("#city").value
+    const gender = document.querySelector('input[name="gender"]:checked')
+    const hobbies = document.querySelectorAll('#hobbies:checked')
+    const address = document.querySelector("#address").value
+
+    //////All error  Data 
+    const name_error = document.querySelector("#name_error")
+    const email_error = document.querySelector("#email_error")
+    const mobile_error = document.querySelector("#mobile_error")
+    const city_error = document.querySelector("#city_error")
+    const gender_error = document.querySelector('#gender_error')
+    const hobbies_error = document.querySelector('#hobbies_error')
+    const address_error = document.querySelector("#address_error")
+
+
+    let arr = [];
+    hobbies?.forEach((hobby) => {
+        arr.push(hobby.value)
+    });
+    // console.log(arr);
+
+    const StringRegEx = /^[A-Za-z. ]*$/;
+    // const  StringRegEx  = /^[A-Za-z. ]{2,}$/;
+    // const StringRegEx = /^[A-Za-z. ]{2,15}$/;
+
+    // const MobileRegEx = /^[0-9]{10}$/
+    const MobileRegEx = /^[6789][0-9]{9}$/
+
+
+
+    if (!name.trim()) {
+        name_error.innerHTML = "please enter your name"
+    } else if (!StringRegEx.test(name)) {
+        name_error.innerHTML = "please enter only char"
+    }
+    else {
+        name_error.innerHTML = ""
+    }
+
+
+    if (!email.trim()) {
+        email_error.innerHTML = "please enter your name"
+    } else {
+        email_error.innerHTML = ""
+    }
+
+
+    if (!mobile.trim()) {
+        mobile_error.innerHTML = "please enter your name"
+    } else if(!MobileRegEx.test(mobile)){
+      mobile_error.innerHTML = "please enter only number"
+    }
+    else {
+        mobile_error.innerHTML = ""
+    }
+
     
+    if (!city) {
+        city_error.innerHTML = "please enter your name"
+    } else {
+        city_error.innerHTML = ""
+    }
 
+    if (!gender) {
+        gender_error.innerHTML = "please enter your name"
+    } else {
+        gender_error.innerHTML = ""
+    }
 
-//////All input  Data 
-const name = document.querySelector("#name").value
-const email = document.querySelector("#email").value
-const mobile = document.querySelector("#mobile").value
-const city = document.querySelector("#city").value
-const gender = document.querySelector('input[name="gender"]:checked')
-const hobbies = document.querySelectorAll('#hobbies:checked')
-const address = document.querySelector("#address").value
+    if (!address.trim()) {
+        address_error.innerHTML = "please enter your name"
+    } else {
+        address_error.innerHTML = ""
+    }
 
-//////All error  Data 
-const name_error = document.querySelector("#name_error")
-const email_error = document.querySelector("#email_error")
-const mobile_error = document.querySelector("#mobile_error")
-const city_error = document.querySelector("#city_error")
-const gender_error = document.querySelector('#gender_error')
-const hobbies_error = document.querySelector('#hobbies_error')
-const address_error = document.querySelector("#address_error")
-
-
-let arr =[];
-hobbies?.forEach((hobby) => {
-    arr.push (hobby.value)
-});
-// console.log(arr);
-
-
-if (!name.trim()) {
-    name_error.innerHTML = "please enter your name"
-} else {
-    name_error.innerHTML = ""
-}
-
-if (!email.trim()) {
-    email_error.innerHTML = "please enter your name"
-} else {
-   email_error.innerHTML = ""
-}
-
-if (!mobile.trim()) {
-    mobile_error.innerHTML = "please enter your name"
-} else {
-   mobile_error.innerHTML = ""
-}
-
-if (!city) {
-    city_error.innerHTML = "please enter your name"
-} else {
-   city_error.innerHTML = ""
-}
-
-if (!gender) {
-    gender_error.innerHTML = "please enter your name"
-} else {
-    gender_error.innerHTML = ""
-}
-
-if (!address.trim()) {
-    address_error.innerHTML = "please enter your name"
-} else {
-    address_error.innerHTML = ""
-}
-
-if (hobbies.length>0){
-    hobbies_error.innerHTML=""
-}else{
-   hobbies_error.innerHTML="enter your hobbies" 
-}
+    if (hobbies.length > 0) {
+        hobbies_error.innerHTML = ""
+    } else {
+        hobbies_error.innerHTML = "enter your hobbies"
+    }
 })
